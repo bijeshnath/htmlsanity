09b6813c89f9cdf659fcdac03144a842dde34db8
Bijesh Ravindranath <bijeshnath@gmail.com>
2013-02-04 21:46:00 +0000

    First Commity

---
+++ src/HtmlSanity/RemoveNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HtmlSanity
+{
+    public class RemoveNode
+    {
+        public string nodeName;
+        public string attrib;
+        public string attribVal;
+        public RemoveNode(string name, string attrib, string attribval)
+        {
+            this.nodeName = name;
+            this.attrib = attrib;
+            this.attribVal = attribval;
+        }
+    }
+}
---
+++ src/HtmlSanity/Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using HtmlAgilityPack;
+
+namespace HtmlSanity
+{
+    [TestFixture]
+    public class Test
+    {
+        [TestCase]
+        public static void TestRemoveNodeFromUrl()
+        {
+            String url = "http://www.bbc.co.uk/news/";
+            HtmlSanitiser sn = new HtmlSanitiser(url);
+            RemoveNode rn1 = new RemoveNode("script", null, null);
+            sn.RemoveNodes(new RemoveNode[] { rn1 });
+
+            string htmlout = sn.Html;
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(htmlout);
+
+            bool foundNode = false;
+            if (doc.DocumentNode.Name == "script")
+                foundNode = true;
+            if(doc.DocumentNode.HasChildNodes)
+            {
+                foreach(HtmlNode cNode in doc.DocumentNode.ChildNodes)
+                    if (cNode.Name == "script")
+                    {
+                        foundNode = true;
+                    }
+            }
+
+            if (htmlout.Contains("<script"))
+            {
+                foundNode = true;
+            }
+
+            Assert.AreEqual(foundNode, false);
+        }
+    }
+}
