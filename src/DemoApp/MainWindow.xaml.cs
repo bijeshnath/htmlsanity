ec47bf79c0c508406caa6f639f79d0bfd6ffa5c0
Bijeshnath <bijeshnath@gmail.com>
2013-04-21 22:17:07 +0100

    An html utils project

---
+++ src/DemoApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IDictionary<string, string> images;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HtmlUtils.DownloadImages di = new HtmlUtils.DownloadImages(txtUrl.Text, @"C:\Users\Home\Documents\Reader\images\");
+            images = di.Download();
+            di.Document.Save(@"C:\Users\Home\Documents\Reader\images\out.html");
+        }
+    }
+}
---
+++ src/HtmlUtils/DownloadImages.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace HtmlUtils
+{
+    public class DownloadImages
+    {
+        HtmlAgilityPack.HtmlDocument doc;
+        string path;
+        string link;
+        public DownloadImages(string arglink, string argpath)
+        {
+            path = argpath;
+            link = arglink;
+            doc = new HtmlAgilityPack.HtmlDocument();
+
+        }
+        public HtmlDocument Document
+        {
+            get
+            {
+                return doc;
+            }
+        }
+
+        public IDictionary<string, string> Download(String strHtml)
+        {
+            List<String> fileNames = new List<string>();
+
+            doc.LoadHtml(strHtml);
+            int i = 0;
+            var images = new Dictionary<string, string>();
+            HtmlNode curNode = doc.DocumentNode;
+            i = downLoadImagesFromChildNodes(curNode, i, images);
+
+            return images;
+
+        }
+
+
+        public IDictionary<string, string> Download()
+        {
+            List<String> fileNames = new List<string>();
+            WebClient wc = new WebClient();
+            string str = wc.DownloadString(link);
+            doc.LoadHtml(str);
+            int i = 0;
+            var images = new  Dictionary<string, string>();
+            HtmlNode curNode = doc.DocumentNode;
+            i = downLoadImagesFromChildNodes(curNode, i, images );
+
+            return images;
+
+        }
+        private int downLoadImagesFromChildNodes(HtmlNode node, int i, IDictionary<string, string> images)
+        {
+
+            if (node.HasChildNodes)
+            {
+                foreach (HtmlNode childNode in node.ChildNodes)
+                {
+                    if (childNode.Name == "img")
+                    {
+                        String src = childNode.GetAttributeValue("src", "---+++");
+                        if (src != "---+++")
+                        {
+                            int qIndex = src.IndexOf('?');
+                            if(qIndex>0)
+                                src = src.Substring(0, qIndex);
+                            string ext = System.IO.Path.GetExtension(src);
+                            WebClient wc = new WebClient();
+                            string outFile = path + "img" + i + ext;
+                            wc.DownloadFile("http:" + src, outFile );
+                            if(!images.ContainsKey(src))
+                                images.Add(src, outFile);
+                            childNode.SetAttributeValue("src", outFile);
+                            i++;
+                        }
+                    }
+                    i = downLoadImagesFromChildNodes(childNode, i, images);
+                }
+
+            }
+
+            return i;
+        }
+    }
+}
